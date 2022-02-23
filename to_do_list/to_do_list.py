524308c77a74b83b5c7d5d0c8b60831b933def9d
Pawel Macyszyn <macyszyn78@gmail.com>
2022-02-22 19:08:57 -0800

    The program has been extended by: -Extending the list with status -The division of tasks into categories: Home, Work
    Several changes in functionality and appearance have been added

--- to_do_list/to_do_list.py
+++ to_do_list/to_do_list.py
@@ -1,18 +1,22 @@
 from ast import Pass
+from ctypes import FormatError
 import os
 import sys  # end all program
 
 from posixpath import split
 tasks = []  # list [task_row_1, task_row2 ..]
 
-# tasks = [['Granie w gry', '6'], ['Pracowanie na dematic', '1']]
+# Format tasks
+# [2] Pisanie kodu        Priority: (6)    Place: HOME     Status: IN PROGRES
 
 
 def show_tasks():
     if len(tasks) > 0:
         for num, task in enumerate(tasks):
             print("[" + str(num) + "] " + task[0] +
-                  "\t Priority: (" + task[1] + ")")
+                  "\tPriority: (" + task[1] + ")" +
+                  "\t Place: " + task[2] +
+                  "\t Status: " + task[3])
     else:
         print('There is no tasks')
 
@@ -28,61 +32,99 @@ def add_task():
         task = input("Choose priority: <1 .. 6>")
     task_row.append(task)
 
+    task = input("Choose place work or home:  W / H: ")
+    while (not(task.upper() == "W" or task.upper() == "H")):
+        print("Invalid selection!")
+        task = input("Choose place work or home:  W / H: ")
+
+    if(task.upper() == "W"):
+        task_row.append("WORK")
+    else:
+        task_row.append("HOME")
+
+    task = input(
+        "Choose status \"done\" / \"in progres\" / \"to do\"  D / I / T: ")
+    while (not(task.upper() == "D" or task.upper() == "I" or task.upper() == "T")):
+        print("Invalid selection!")
+        task = input(
+            "Choose status \"done\" / \"in progres\" / \"to do\"  D / I / T: ")
+
+    if(task.upper() == "D"):
+        task_row.append("DONE")
+
+    elif(task.upper() == "I"):
+        task_row.append("IN PROGRES")
+
+    else:
+        task_row.append("TO DO")
+
     tasks.append(task_row)
     print("Task was added !")
 
 
 def delete_task():
-    delete_task_exit = True
-    while(delete_task_exit):
-        print("Write \"SHOW\" to display the tasks")
-        print("Press \"R\" to exit")
+    if len(tasks) > 0:
+        delete_task_exit = True
+        while(delete_task_exit):
+            print("Write \"SHOW\" to display the tasks")
+            print("Press \"R\" to exit")
 
-        task_indx = input("Enter the index of the task to be deleted: ")
+            task_indx = input("Enter the index of the task to be deleted: ")
 
-        if (task_indx.isnumeric() == True):
-            task_indx = int(task_indx)
-            os.system('cls')
-            if task_indx >= 0 and task_indx < len(tasks):
-                tasks.pop(task_indx)
-                print("Task was deleted\n")
+            if (task_indx.isnumeric() == True):
+                task_indx = int(task_indx)
+                os.system('cls')
+                if task_indx >= 0 and task_indx < len(tasks):
+                    tasks.pop(task_indx)
+                    print("Task was deleted\n")
+                else:
+                    print("!!! A task with this index does not exist !!!\n")
+            elif (task_indx.capitalize() == 'R'):
+                delete_task_exit = False
+                os.system('cls')
+            elif (task_indx.upper() == 'SHOW'):
+                os.system('cls')
+                show_tasks()
+                print()
             else:
-                print("!!! A task with this index does not exist !!!\n")
-        elif (task_indx.capitalize() == 'R'):
-            delete_task_exit = False
-            os.system('cls')
-        elif (task_indx.upper() == 'SHOW'):
-            os.system('cls')
-            show_tasks()
-            print()
-        else:
-            print('task id has to be numeric, provide correct value')
+                print('task id has to be numeric, provide correct value')
+    else:
+        print('There is nothing to remove')
 
 
 def save_tasks_to_file():
-    print("If you want to quit, enter \"QUIT\"")
-    name_file = input("Enter name for file: ")
-    if(not(name_file.upper() == "QUIT")):
-        with open(name_file + ".txt", "w") as file:
-            for task_row in tasks:
-                for task in task_row:
-                    file.write(task + ",    ")
-                file.write("\n")
-        print("Save done")
+    if len(tasks) > 0:
+        print("If you want to quit, enter \"QUIT\"")
+        name_file = input("Enter name for file.txt: ")
+        if(not(name_file.upper() == "QUIT")):
+            with open(name_file, "w") as file:
+                for task_row in tasks:
+                    for task in task_row:
+                        file.write(task + ",    ")
+                    file.write("\n")
+            print("Save done")
+        else:
+            pass
     else:
-        pass
+        print('There is nothing to save')
 
 
 def load_tasks_from_file():
     while(True):
         try:
+            print("---------------------------------------------")
             print("If you wanna quit, press \"Q\"")
-            name_file = input("Indicate the file name: ")
+            print("If you not wanna indicate the file, press \"R\"")
+
+            name_file = input("Indicate the file name.txt: ")
 
             if(name_file.upper() == "Q"):
-                sys.exit(0)      
+                sys.exit(0)
 
-            with open(name_file + ".txt") as file:
+            if(name_file.upper() == "R"):
+                break
+
+            with open(name_file) as file:
                 lines = file.readlines()
             print("File was opened: ")
 
@@ -92,6 +134,7 @@ def load_tasks_from_file():
             break
         except FileNotFoundError:
             print('No previous tasks loaded')
+
 
 def try_again_message():
     print("\n Wrong value. try again... \n")
@@ -135,7 +178,7 @@ def automatic_save_on_exit():
 
 # ----------------my-program---------------------------
 load_tasks_from_file()
-
+os.system('cls')
 user_choice = 0
 print_menu()
 while(user_choice != 5):
