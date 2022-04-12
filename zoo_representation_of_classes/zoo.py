798ab2b1f3d6d486fdbb5de5e6cabf96cdf8d000
Pawel Macyszyn <macyszyn78@gmail.com>
2022-04-12 19:56:29 -0400

    Presenting an abstraction

--- zoo_representation_of_classes/zoo.py
+++ zoo_representation_of_classes/zoo.py
@@ -1,12 +1,19 @@
-class Zwierze():
+from abc import ABC, abstractmethod
+
+
+class Zwierze(ABC):
     def __init__(self, nazwa, wiek, waga):
         self.nazwa = nazwa
         self.wiek = wiek
         self.waga = waga
 
+    @abstractmethod # tutaj wymuszamy implementację tej metody w klasach pochodnych
+    def nazwa_gatunku(self):
+        pass
+    
     def przedstaw_sie(self):
         print(
-            f"Jestem zwierzeciem {self.nazwa}, mam {self.wiek} lat oraz wage {self.waga} kg.")
+            f"Jestem {self.nazwa_gatunku()}, Mam na imię {self.nazwa}, mam {self.wiek} lat oraz wage {self.waga} kg.")
 
     def urodziny(self):
         self.wiek += 1
@@ -16,13 +23,18 @@ class Zwierze():
 
 
 class Slon(Zwierze):
-    def przedstaw_sie(self):
-        print(
-            f"Jestem słoniem {self.nazwa}, mam {self.wiek} lat oraz wazę {self.waga} kg.")
+    def nazwa_gatunku(self):
+        return "Slon"
     pass
 
 
 class Papuga(Zwierze):
+    def nazwa_gatunku(self):
+        return "Papuga"
+    pass
+
+
+
     def __init__(self, nazwa, wiek, waga, kolor):
         super().__init__(nazwa, wiek, waga)
         self.kolor = kolor
@@ -30,47 +42,41 @@ class Papuga(Zwierze):
     def przedstaw_sie(self):
         super().przedstaw_sie()
         print(f"Jako papuga mój kolor to {self.kolor}")
-    
 
 
 class Lew(Zwierze):
-    def przedstaw_sie(self):
-        super().przedstaw_sie()
-        print("A tak w ogóle to jestem lwem")
+    def nazwa_gatunku(self):
+        return "Lew"
     pass
-
-
 
 
 def nowy_rok(zoo):
     for zwierze in zoo:
-        zwierze.urodziny()   
+        zwierze.urodziny()
+
 
 def przedstaw_zwierzeta(zoo):
     for zwierze in zoo:
         zwierze.przedstaw_sie()
 
 
-
 def main():
     Dumboo = Slon("Dumboo", 77, 6000)
     Simba = Lew("Simba", 24, 100)
-    jakis_zwierz = Zwierze("Pawel", 29, 69)
+    # jakis_zwierz = Zwierze("Pawel", 29, 69)
     Jago = Papuga("Jago", 32, 3, "czerwony")
 
-    # Dumboo.przedstaw_sie()
-    # Simba.przedstaw_sie()
+    Dumboo.przedstaw_sie()
+    Simba.przedstaw_sie()
     # jakis_zwierz.przedstaw_sie()
 
-    # Jago.urodziny()
-    # Jago.grubasek()
-    # Jago.przedstaw_sie()
+    Jago.urodziny()
+    Jago.grubasek()
+    Jago.przedstaw_sie()
 
-    zoo = [Dumboo, Simba, Jago, jakis_zwierz]
-    nowy_rok(zoo)
-    przedstaw_zwierzeta(zoo)
-
-
+    # zoo = [Dumboo, Simba, Jago, jakis_zwierz]
+    # nowy_rok(zoo)
+    # przedstaw_zwierzeta(zoo)
 
 
 if __name__ == "__main__":
