ed6b1a09f747d42cebf319f0a90ee26cea181a79
Pawel Macyszyn <macyszyn78@gmail.com>
2022-04-20 16:31:05 -0400

    Adding method count_perimeter

--- representation_of_classes/geometric_figures_classes.py
+++ representation_of_classes/geometric_figures_classes.py
@@ -1,20 +1,22 @@
-# to do
-# make classes for  triangles
-
-from cmath import pi
-
+import math
 
 class Circle():
     def __init__(self, radius) -> None:
         self.radius = radius
         self.count_surface_area()
+        self.count_perimeter()
 
     def count_surface_area(self):
-        self.surface_area = pi * (self.radius**2)
+        self.surface_area = math.pi * (self.radius**2)
+        pass
+
+    def count_perimeter(self):
+        self.perimeter = 2 * self.radius * math.pi
         pass
 
     def print_surface_area(self):
-        print(self.__class__.__name__, ":", self.surface_area, "m^2")
+        print(self.__class__.__name__, ":", "%3.2f" % self.surface_area, "m^2")
+        print("%3.2f" % self.perimeter, "m")
         pass
 
 
@@ -22,10 +24,15 @@ class Squares(Circle):
     def __init__(self, side_a) -> None:
         self.side_a = side_a
         self.count_surface_area()
+        self.count_perimeter()
         pass
 
     def count_surface_area(self):
         self.surface_area = self.side_a * self.side_a
+        pass
+
+    def count_perimeter(self):
+        self.perimeter = 4 * self.side_a
         pass
 
 
@@ -34,22 +41,38 @@ class Rectangle(Squares):
         self.side_a = side_a
         self.side_b = side_b
         self.count_surface_area()
+        self.count_perimeter()
         pass
 
     def count_surface_area(self):
         self.surface_area = self.side_a * self.side_b
         pass
 
+    def count_perimeter(self):
+        self.perimeter = 2 * self.side_a * 2 * self.side_b
+        pass
 
-class Triangle(Circle):
+
+class RightTriangle(Circle):
     def __init__(self, side_a, side_h) -> None:
         self.side_a = side_a
         self.side_h = side_h
         self.count_surface_area()
+        self.count_perimeter()
         pass
 
     def count_surface_area(self):
         self.surface_area = self.side_a * self.side_h / 2
+        pass
+
+    def count_perimeter(self):
+        if self.side_a == self.side_h:
+            self.perimeter = self.side_a + \
+                self.side_a + (self.side_a * math.sqrt(2))
+        else:
+            self.side_c = math.sqrt(
+                self.side_a*self.side_a + self.side_h*self.side_h)
+            self.perimeter = self.side_c + self.side_a + self.side_h
         pass
 
 
@@ -62,5 +85,5 @@ x2.print_surface_area()
 x3 = Rectangle(2, 3)
 x3.print_surface_area()
 
-x4 = Triangle(2, 3)
+x4 = RightTriangle(2, 3)
 x4.print_surface_area()
