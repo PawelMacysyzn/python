59dfc2511e66b0815c244f1efbdd7851e24a4b5e
Pawel Macyszyn <macyszyn78@gmail.com>
2022-09-01 22:32:49 +0200

    Examples and answers with tasks

---
+++ api/api.py
@@ -0,0 +1,20 @@
+import json
+import requests
+import secrets
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+
+headers = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': secrets.API_KEY,
+}
+
+
+response = requests.get(url, headers=headers)
+
+
+if (response.status_code != requests.codes.ok):
+    print('Error!')
+else:
+    # print(response.status_code)
+    print(json.dumps(response.json(), indent=2))
