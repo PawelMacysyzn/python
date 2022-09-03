ca54a8375ed973fa4b73d3f361947a91dd08b5a9
Pawel Macyszyn <macyszyn78@gmail.com>
2022-09-03 21:11:56 +0200

    Examples and answers with tasks

---
+++ test/orgin_1.py
@@ -0,0 +1,29 @@
+'''
+Prepare the functions to check if the person of the given age is of legal age
+
+EN/PL
+
+Przygotuj funkcje sprawdzającą czy osoba o przekazanym wieku jest osoba pełnoletnią 
+
+'''
+
+
+def is_adult(age):
+    return age >= 18
+
+
+def test_is_adult():
+    # given
+    age = 18
+
+    # when
+    result = is_adult(age)
+
+    # then
+    assert result == True
+    assert is_adult(20)
+
+
+def test_is_not_adult():
+    assert not is_adult(17)
+    assert not is_adult(3)
