3341e49118c9e9a4d3b3fef6affa22617b1e0b92
Pawel Macyszyn <macyszyn78@gmail.com>
2022-09-17 12:54:16 +0200

    Training

---
+++ Szkolenie_embedded/task_2.py
@@ -0,0 +1,26 @@
+# Zaimplementuj program, który przygotowuje środowisko do pracy. Uruchom 5 dowolnych procesów
+
+
+import subprocess
+import os
+import time
+
+
+def startCalc(path):
+    try:
+        x = subprocess.Popen(path)
+        x.wait()
+    except Exception as exc:
+        print('Errrr..{} \n with: {}'.format(exc, path))
+
+
+# startCalc('Notepad')
+# startCalc('putty')
+# startCalc('calc.exe')
+
+# code = r'C:\Users\pmacyszyn_adm\AppData\Local\Programs\Microsoft VS Code\Code.exe'
+# startCalc(code)
+
+chrome = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+startCalc(chrome)
+
