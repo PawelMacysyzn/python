ca2d69334b729bde7337f1adaa3750254f850294
Pawel Macyszyn <macyszyn78@gmail.com>
2022-09-17 15:47:08 +0200

    Training

--- Szkolenie_embedded/task_4_obfuscate.py
+++ Szkolenie_embedded/task_4_obfuscate.py
@@ -1,4 +1,6 @@
 # Wybierz jedno z wyliczonych narzędzi i użyj na swoim wybranym kodzie źródłowym.
 
+# instrukcja 
+# https://pypi.org/project/pyarmor/
 
 # C:\Users\pmacyszyn_adm\Documents\python\python\Szkolenie_embedded\dist_pyarmor_obfuscated_scripts
---
+++ Szkolenie_embedded/task_5_convert_to_c.py
@@ -0,0 +1,11 @@
+# Przekształacanie pliku z *.py do *.c
+
+
+# instrukcja 
+# https://medium.com/@xpl/protecting-python-sources-using-cython-dcd940bb188e
+
+# C:\Users\pmacyszyn_adm\Documents\python\python\Szkolenie_embedded\compile.py
+# C:\Users\pmacyszyn_adm\Documents\python\python\Szkolenie_embedded\main.py
+# C:\Users\pmacyszyn_adm\Documents\python\python\Szkolenie_embedded\task_2_subprocess.c
+
+
